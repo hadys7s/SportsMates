d665ab4ad9fa342d5b88fcd85368c6b48e3d6374
Hady Hessen <hady815@gmail.com>
2021-01-31 14:14:38 +0200

    - project Structure

--- app/src/main/java/com/example/sportsmates/Home/MainActivity.kt
+++ app/src/main/java/com/example/sportsmates/Home/MainActivity.kt
@@ -1,12 +1,12 @@
-package com.example.sportsmates
+package com.example.sportsmates.Home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sportsmates.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        "hffi"
     }
 }
