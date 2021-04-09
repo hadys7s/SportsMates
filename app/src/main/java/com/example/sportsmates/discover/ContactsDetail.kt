86a5d05f89ba0d7107c5dd74532728e330dd96db
Hady ZainEldeen <hady.zaineldeen@areebgroup.com>
2021-04-09 18:00:23 +0200

    - ChangeLog.md + version number

--- app/src/main/java/com/example/sportsmates/discover/ContactsDetail.kt
+++ app/src/main/java/com/example/sportsmates/discover/ContactsDetail.kt
@@ -17,7 +17,6 @@ import org.koin.android.viewmodel.ext.android.viewModel
 
 class ContactsDetail : AppCompatActivity() {
     private lateinit var binding: ActivityContactsDetailBinding
-    private val viewModel: ContactsViewModel by viewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityContactsDetailBinding.inflate(layoutInflater)
